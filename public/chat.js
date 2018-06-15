f3893c70fa5ecea7e6de7cca743f6bdd84f6340a
Kevin Klatt <klattkev@gmail.com>
2018-06-15 22:05:39 +0200

    links added

--- public/chat.js
+++ public/chat.js
@@ -18,7 +18,7 @@ var message = document.getElementById('message'),
 
 //user-EMOTES
 var peepoHappy = "<embed src='https://assets.change.org/photos/2/io/gq/skIogQbuyLePLdd-800x450-noPad.jpg?1527916573' width='30' height='30' />",
-    TriHard = "<embed src='https://ih0.redbubble.net/image.501881470.8713/flat,800x800,070,f.u2.jpg' width='30' height='30' />",
+    TriHard = "<embed src='https://i.redd.it/z7b7otx1f5zy.png' width='30' height='30' />",
     TriHardVid = "<embed src='https://www.youtube.com/embed/9f9KzWNqKGo?autoplay=1' width='640' height='360' />";
     cmonBruh = "<embed src='https://static-cdn.jtvnw.net/jtv_user_pictures/cmonbruh-profile_image-84cf1a6644b6e42a-300x300.png' width='30' height='30' />"
 
@@ -39,9 +39,7 @@ var peepoHappy = "<embed src='https://assets.change.org/photos/2/io/gq/skIogQbuy
         }
     }
 
-    function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
+
 
 
 var mySound;
@@ -99,17 +97,49 @@ socket.on('getCount', function(total) {
 // Listen for events
 socket.on('chat', function(data){
   feedback.innerHTML = "";
-  newMes = data.message.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+
+  newMes = data.message
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/TriHard/g, TriHard)
+  .replace(/peepoHappy/g, peepoHappy)
+  .replace(/cmonBruh/g, cmonBruh);
+
+
   newName = data.handle.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+
+
 
 
 
   if (data.handle == "") {
 
-    output.innerHTML += '<p><strong><span style="color:orange">' + 'user' + random_number + ': </strong></span>' + newMes + '</p>' + "\n";
+    var n = (newMes.includes(".c") || newMes.includes(".d") || newMes.includes("http:") || newMes.includes("https:") || newMes.includes("www."));
+
+
+    if (n == true) {
+      output.innerHTML += '<p><strong><span style="color:orange">' + 'user' + random_number + ': </strong></span>' + '<a style="color:white" target="_blank" href="' + newMes + '">' + newMes +'</a>' + "\n";
+
+    } else {
+      output.innerHTML += '<p><strong><span style="color:orange">' + 'user' + random_number + ': </strong></span>' + newMes + '</p>' + "\n";
+    }
+
+
+
   } else {
 
-    output.innerHTML += '<p><strong><span style="color:red">' + newName + ': </strong></span>' + newMes + '</p>' + "\n";
+    var n = (newMes.includes(".c") || newMes.includes(".d") || newMes.includes("http:") || newMes.includes("https:") || newMes.includes("www."));
+
+
+    if (n == true) {
+      output.innerHTML += '<p><strong><span style="color:red">' + newName + ': </strong></span>' + '<a style="color:white" target="_blank" href="' + newMes + '">' + newMes +'</a>' + "\n";
+
+    } else {
+      output.innerHTML += '<p><strong><span style="color:red">' + newName + ': </strong></span>' + newMes + '</p>' + "\n";
+    }
+
 
   };
 
@@ -125,21 +155,10 @@ socket.on('chat', function(data){
     output.innerHTML += "Bot: " + (Math.floor(Math.random() * 99) + 1) + "\n";
   }
 
-  if (data.message == "peepoHappy") {
-    output.innerHTML += peepoHappy;
-  }
-
-  if (data.message == "TriHard") {
-    output.innerHTML += TriHard;
-  }
-
   if (data.message == "TriHardVid") {
     output.innerHTML += TriHardVid;
   }
 
-  if (data.message == "cmonBruh") {
-    output.innerHTML+= cmonBruh;
-  }
 
   if (data.message == "!hug") {
     output.innerHTML += "Bot: " + "I wuuve you *HUG* " + peepoHappy + "\n";
@@ -154,6 +173,10 @@ socket.on('chat', function(data){
 
 
 socket.on('typing', function(data) {
+
+
+
+
   newHandle = data.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   feedback.innerHTML = '<p><em>' + newHandle + ' is typing a message...</em></p>';
 
